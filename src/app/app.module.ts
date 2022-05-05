459ce0a083f83f0ae7a613e795c0ee6ce69994eb
waltermolina <waltermolina@msn.com>
2022-05-05 18:27:14 -0300

    Armado de Esqueleto y Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { SocialLinkComponent } from './components/social-link/social-link.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    SocialLinkComponent
   ],
   imports: [
     BrowserModule,

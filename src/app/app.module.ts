2b5822e32f001e4f34f753af6463005acb3ce34d
andy <94704724+pouder697@users.noreply.github.com>
2022-05-06 15:39:42 -0300

    Corrección error tipeo ProfileComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { SkillsComponent } from './componentes/skills/skills.component';
     HeaderComponent,
     NavComponent,
     AboutComponent,
-    PrdofileComponent,
+    ProfileComponent,
     SocialLinkComponent,
     ExperienciaComponent,
     EducacionComponent,

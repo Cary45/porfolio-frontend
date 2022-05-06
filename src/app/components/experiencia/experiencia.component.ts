a4d7a93937d8d2b2fe4ca94cc5df66a011200c82
Martin <tincho.info@gmail.com>
2022-05-06 17:30:56 -0300

    cambios visuales en experiencia

--- src/app/components/experiencia/experiencia.component.ts
+++ src/app/components/experiencia/experiencia.component.ts
@@ -6,27 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiencia.component.css'],
 })
 export class ExperienciaComponent implements OnInit {
-  constructor() {}
+  constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
   Experiencia: any[] = [
     {
       idlaboral: 1,
-      puesto: 'dios',
-      descripcion: 'todo',
-      empresa: 'nada',
-      inicio: null,
-      fin: null,
+      puesto: 'Informatico',
+      descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+      empresa: 'Argentina',
+      inicio: '10/10/10',
+      fin: '10/10/10',
       fotourl: "https://via.placeholder.com/150",
       persona: 1,
     },
-      {
+    {
       idlaboral: 1,
-      puesto: 'dios1',
-      descripcion: 'todo1',
-      empresa: 'nada2',
-      inicio:" ayer",
-      fin: "hoy",
+      puesto: 'Informatico2',
+      descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+      empresa: 'Argentina',
+      inicio: '09/09/09',
+      fin: '09/09/09',
       fotourl: "https://via.placeholder.com/150",
       persona: 1,
     },

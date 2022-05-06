90a51f858644e4d7408c9300fc65e6a56344f5a5
Camilagrob <camilagrobert@gmail.com>
2022-05-06 02:08:10 -0300

    Agregamos el componente Proyectos

--- src/app/components/proyectos/proyectos.component.ts
+++ src/app/components/proyectos/proyectos.component.ts
@@ -9,7 +9,25 @@ export class ProyectosComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-}
+    
+    Proyectos: any[] = [
+
+      {
+        idproyecto: 1,
+        proyecto: 'Proyecto 1',
+        descrip: 'Esto es una descripción del proyecto 1.Lorem Ipsum is simply dummy tlkdfñldklñfkñldk slfk ñsalfksalñkf sñalkf salñkfsa lñkfñlak af lakñl klsñk lñk a slñk lask lfk f añflkas lñkfaslñk lk as añlksañl kflñ kaa afklñ kñlsk lñk  ñlfklñsk ñlsk alñ k ñlkflñsklskfsñ k',
+        url: 'https://github.com',
+        fotourl: "https://global-uploads.webflow.com/5f5a53e153805db840dae2db/6073fbf151fa4565d48572dc_GitHub_aprender-programaci%25C3%25B3n.jpeg",
+      },
+      {
+        idproyecto: 2,
+        proyecto: 'Proyecto 2',
+        descrip: 'Esto es una descripción del proyecto 2',
+        url: 'https://github.com',
+        fotourl: "https://global-uploads.webflow.com/5f5a53e153805db840dae2db/6073fbf151fa4565d48572dc_GitHub_aprender-programaci%25C3%25B3n.jpeg",
+      },
+      
+    ];
+  }

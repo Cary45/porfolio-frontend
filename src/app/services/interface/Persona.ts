c8a13ee70d7184cd1765ebef5b125c73702671b0
Nicolas <nicogamarra2010@gmail.com>
2022-05-08 16:01:21 -0300

    persona interface

---
+++ src/app/services/interface/Persona.ts
@@ -0,0 +1,15 @@
+export interface Persona {
+
+    idpersona: number;
+    nombre: String;
+    apellido: String;
+    email: String;
+    titulo: String;
+    telefono: String;
+    ubicacion: String;
+    acerca: String;
+    fotourl: String;
+    username: String;
+    password: String;
+
+}

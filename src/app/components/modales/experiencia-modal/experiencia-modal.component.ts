f02858ae6f4bce83aff5bfd8fd43eb873b40e74e
nanreh1976 <hernanchogomez@gmail.com>
2022-05-17 01:57:49 -0300

    from experiencia

--- src/app/components/modales/experiencia-modal/experiencia-modal.component.ts
+++ src/app/components/modales/experiencia-modal/experiencia-modal.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Laboral } from 'src/app/services/interface/Laboral';
+import { ExperienciaService } from 'src/app/services/api-rest/experiencia.service';
 
 @Component({
   selector: 'app-experiencia-modal',
@@ -10,12 +13,64 @@ export class ExperienciaModalComponent implements OnInit {
 
   @Input()  id!:number; //recibe el id del elemento que se quiere editar
 
-  constructor(public activeModal: NgbActiveModal) { }
+  experiencia!:Laboral;
+  formulario!:FormGroup
+  constructor(public activeModal: NgbActiveModal, private experienciaService:ExperienciaService, private fb: FormBuilder) {
+    this.formulario = this.fb.group({
+      idelaboral: [''],
+      puesto: [''],
+      descripcion: [''],
+      empresa: [''],
+      inicio: [''],
+      fin: [''],
+      fotourl: [''],
+      persona: ['']
+    })
+    /*  esto lo saque de la interface Educacion 
+    idlaboral?: number;
+    puesto: String;
+    descripcion: String;
+    empresa: String;
+    inicio: Date;
+    fin: Date;
+    fotourl: String;   
+    persona: number;
+    */
+  
+  
+  
+  
+  }
 
-  ngOnInit(): void {
+   ngOnInit(): void {
+      this.getById(this.id)
   }
 
   cerrarModal(){
     this.activeModal.close();
   }
+  getById(id: number) {
+    //console.log(this.id)
+    this.experienciaService.getById(id).subscribe (
+            data => {
+         this.experiencia = data; 
+         //console.log(this.experiencia)
+         this.editarForm(this.experiencia)
+        }
+        );
+
+  }
+  editarForm(exp:any){
+    this.formulario.setValue( {
+      idlaboral: exp.idelaboral,
+      puesto: exp.puesto,
+      descripcion: exp.descripcion,
+      empresa: exp.empresa,
+      inicio: exp.inicio,
+      fin: exp.fin,
+      fotourl: exp.fotourl,
+      persona: exp.persona
+    });
+  }
+  
 }

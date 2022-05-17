824f2bd135b32b9445a06b827414c204342eb3ed
nanreh1976 <hernanchogomez@gmail.com>
2022-05-17 01:15:19 -0300

    form proyectos

--- src/app/components/modales/proyecto-modal/proyecto-modal.component.ts
+++ src/app/components/modales/proyecto-modal/proyecto-modal.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Proyecto } from 'src/app/services/interface/Proyecto';
+import { ProyectosService } from 'src/app/services/api-rest/proyectos.service';
 
 @Component({
   selector: 'app-proyecto-modal',
@@ -11,12 +14,54 @@ export class ProyectoModalComponent implements OnInit {
   
   @Input()  id!:number; //recibe el id del elemento que se quiere editar
 
-  constructor(public activeModal: NgbActiveModal) { }
+  proyecto!:Proyecto;
+  formulario!:FormGroup
+  constructor(public activeModal: NgbActiveModal, private proyectoService:ProyectosService, private fb: FormBuilder) {
+    this.formulario = this.fb.group({
+      ideproyecto: [''],
+      titulo: [''],
+      descripcion: [''],
+      url: [''],
+      persona: [''],
+    })
+   /*  esto lo saque de la interface proyecto 
+    ideproyecto?: number;
+    titulo: String;
+    descripcion: string;
+    url: String; 
+    persona: number;
+    */
+  }
+
+
+
 
   ngOnInit(): void {
+    this.getById(this.id)  
   }
 
   cerrarModal(){
     this.activeModal.close();
   }
+  getById(id: number) {
+    console.log(this.id)
+    this.proyectoService.getById(id).subscribe (
+            data => {
+         this.proyecto = data; 
+         console.log(this.proyecto)
+         this.editarForm(this.proyecto)
+        }
+        );
+
+  }
+  editarForm(pro:any){
+    this.formulario.setValue( {
+      ideproyecto: pro.ideproyecto,
+      titulo: pro.titulo,
+      descripcion: pro.institucion,
+      url: pro.institucionurl,
+      persona: pro.persona
+    });
+  }
+
 }

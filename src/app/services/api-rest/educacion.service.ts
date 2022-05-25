b2661a8bb8b5fd792a9052cd5c95b61717610567
cary45 <carinagaray768@gmail.com>
2022-05-25 19:50:00 -0300

    canbio de heroku

--- src/app/services/api-rest/educacion.service.ts
+++ src/app/services/api-rest/educacion.service.ts
@@ -7,30 +7,29 @@ import { Observable } from 'rxjs';
 })
 export class EducacionService {
 
-  url2: string = "https://comunidad-apirest.herokuapp.com/api";
-  url: string = "https://error-pueblada-api.herokuapp.com/api"
+  url: string = "https://proyectoap1-cary.herokuapp.com/api"
   
   constructor(private http: HttpClient) {}
 
   getById(id: number): Observable<any> {
-	  return this.http.get(this.url2 + `/educacion/${id}`);
+	  return this.http.get(this.url + `/educacion/${id}`);
 	}
   getAll(): Observable<any> {
     console.log("pasa por getAll?")
-	  return this.http.get(this.url2+'/educacion');
+	  return this.http.get(this.url+'/educacion');
 	}
   //terminar en algun momento
   update(id: number, skill: any): Observable<any>{
-    return this.http.put(this.url2 + `/educacion/${id}`, skill);
+    return this.http.put(this.url + `/educacion/${id}`, skill);
   }
 
   //terminar en algun momento
   delete(id: number): Observable<any>{
-    return this.http.delete(this.url2 + `/educacion/${id}`);
+    return this.http.delete(this.url + `/educacion/${id}`);
    
   }
  save(skill:any) : Observable<any>{
-   return this.http.post(this.url2 + `/educacion/`, skill);
+   return this.http.post(this.url + `/educacion/`, skill);
  }
 }
 

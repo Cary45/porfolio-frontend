ee365d44933cf3fe80075e05ea65f43c283c3698
cary45 <carinagaray768@gmail.com>
2022-06-11 21:01:46 -0300

    cambios

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,17 +1,28 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { PersonaService } from 'src/app/services/api-rest/persona.service';
+import { Persona } from 'src/app/services/interface/Persona';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.css']
 })
+
 export class BannerComponent implements OnInit {
   @Input() url!: String;
-  constructor () {
-    
-  }
+  persona! : Persona
+  constructor (private personaService : PersonaService){}
+  
 
   ngOnInit(): void {
+    this.actualizarBanner(1);
+
   }
 
+  actualizarBanner (id:number) {
+    this.personaService.getById(id).subscribe
+    (
+      data => { this.persona = data; }
+   )
+  }
 }
